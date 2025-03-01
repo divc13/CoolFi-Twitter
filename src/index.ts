0426e836c44397d41a0006dc0401ab64fbe3cfca
Divyansh Chhabria <chhabriadivyansh@gmail.com>
2025-03-02 04:23:17 +0530

    mention added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { sendChatMessage } from './request';
-import { settings } from './config';
 import "dotenv/config";
 import { handleDM } from './twitter/directMessage';
+import { handleMentions } from './twitter/mention';
 
 async function run() {
     while (true) {
         try {
-            await handleDM(); // Run the function
+            // await handleDM(); 
+            await handleMentions();
         } catch (error) {
-            console.error("Error in handleDM:", error);
+            console.error("Error in handleDM or handleMentions:", error);
         }
         await new Promise(resolve => setTimeout(resolve, 5000)); // Wait for 5 seconds
     }
---
+++ src/twitter/mention.ts
@@ -0,0 +1,42 @@
+import { login } from "./login";
+import { settings } from "../config";
+import { sendChatMessage } from "../request";
+import { SearchMode } from "agent-twitter-client";
+
+var previous_time: any = null;
+const MAX_TWEETS=100;
+
+export async function handleMentions()
+{
+    const scrapper = await login();
+    const current_time= new Date();
+    
+    const query = previous_time? `@${settings.twitterUsername}`: `@${settings.twitterUsername}`;
+
+    const tweetStream = scrapper.searchTweets(query, MAX_TWEETS, SearchMode.Latest);
+    for await (const tweet of tweetStream) 
+    {
+        const tweet_text = tweet.text;
+        const tweetId = tweet.id;
+        const accountID = tweet.userId;
+        const conversationId = tweet.userId + "-" + scrapper.getUserIdByScreenName(settings.twitterUsername);
+
+        console.log(tweet_text);
+
+        var present = false;
+        const mentions = tweet.mentions;
+        for (var mention of mentions)
+        {
+            if (mention.username == settings.twitterUsername) present = true;
+        }
+        if (!present) continue;
+
+        if (accountID && tweetId && tweet_text)
+        {
+            const message_to_send =  await sendChatMessage(accountID, conversationId, tweet_text);
+            await scrapper.sendTweet(message_to_send, tweetId);
+        }
+    }
+
+    previous_time = current_time;
+}
